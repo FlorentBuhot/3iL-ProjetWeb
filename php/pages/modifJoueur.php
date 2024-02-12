be505172cfccd2b9f071039f0b7a87e4ac9f7eba
FlorentBuhot <93909333+FlorentBuhot@users.noreply.github.com>
2024-02-12 11:02:10 +0100

    Add edit to joueur in database

--- php/pages/modifJoueur.php
+++ php/pages/modifJoueur.php
@@ -15,26 +15,29 @@ if (!isset($_SESSION['droit']) || $_SESSION['droit'] != 'admin') {
     </head>
     <body>
     <div class="container mt-5">
+        <?php
+        include_once("../template/inc_header.php");
+        ?>
         <h1 class="mb-4">Modification d'un joueur</h1>
         <form action="/php/controllers/modifierJoueur.php" method="post">
             <div class="form-group">
                 <label for="nom">Nom :</label>
-                <input type="text" class="form-control" id="nom" name="nom" value="<?php echo $_REQUEST['nom']?>">
+                <input required type="text" class="form-control" id="nom" name="nom" value="<?php echo $_REQUEST['nom']?>">
             </div>
 
             <div class="form-group">
                 <label for="prenom">Prénom :</label>
-                <input type="text" class="form-control" id="prenom" name="prenom" value="<?php echo $_REQUEST['prenom']?>">
+                <input required type="text" class="form-control" id="prenom" name="prenom" value="<?php echo $_REQUEST['prenom']?>">
             </div>
 
             <div class="form-group">
                 <label for="nb_match">Nombre de match :</label>
-                <input type="number" class="form-control" id="nb_match" name="nb_match" min="0" value="<?php echo $_REQUEST['nb_match']?>">
+                <input required type="number" class="form-control" id="nb_match" name="nb_match" min="0" value="<?php echo $_REQUEST['nb_match']?>">
             </div>
 
             <div class="form-group">
                 <label for="nb_buts">Nombre de buts total :</label>
-                <input type="number" class="form-control" id="nb_buts" name="nb_buts" min="0" value="<?php echo $_REQUEST['nb_buts']?>">
+                <input required type="number" class="form-control" id="nb_buts" name="nb_buts" min="0" value="<?php echo $_REQUEST['nb_buts']?>">
             </div>
 
             <input hidden id="joueur_id" name="joueur_id" value="<?php echo $_REQUEST['id']?>">
