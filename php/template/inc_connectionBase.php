335385fe4fb16411fd0130479746a1fe01ff080f
FlorentBuhot <93909333+FlorentBuhot@users.noreply.github.com>
2024-02-13 15:39:24 +0100

    Revert "Revert "Init data base""
    This reverts commit 26cd95642c90a4586a55f7dad4cee0f43cb8a0d5.

--- php/template/inc_connectionBase.php
+++ php/template/inc_connectionBase.php
@@ -1,3 +1,20 @@
 <?php
 // Créer une instance de PDO
-$cnx = new PDO("mysql:host=mysql;dbname=tpweb", "tpweb", "tpweb");
+$cnx = new PDO("mysql:host=mysql;dbname=riflonade", "rifloneur", "riflon");
+
+// Test
+/*
+$textReq = "Select * ";
+$textReq .= "from users ";
+
+// Demander la création d'une requête
+$requete = $cnx->prepare($textReq);
+
+// Exécuter la requête
+$requete->execute();
+
+// Get result of request
+$tabRes = $requete->fetchAll();
+
+var_dump($tabRes);
+*/
