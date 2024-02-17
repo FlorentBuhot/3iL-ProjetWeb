0c428f2599f0e33d37fdd947c4db47ca64cbd6b7
Dhaisne <dhaisneclement@gmail.com>
2024-02-17 14:21:52 +0100

    page creation match

--- php/template/inc_header.php
+++ php/template/inc_header.php
@@ -1,3 +1,26 @@
-<nav class="navbar navbar-light bg-light">
-    <span class="navbar-brand mb-0 h1">Staty-Foot</span>
+<?php
+    require_once("../template/inc_head.php");
+?>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            <img src="/img/ballon.png" alt="Logo" width="50" height="50"
+                 class="d-inline-block align-text-top">
+
+        </a>
+
+        <!-- Nom de site-->
+        <span class="navbar-brand fs-2 text-white">Staty-foot</span>
+
+        <!-- Icone utilisateur -->
+        <li class="d-flex nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/img/personne.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+            <ul class="dropdown-menu" style="left: 92% !important">
+                <li><a class="dropdown-item" href="#">Mes informations</a></li>
+                <li><a class="dropdown-item" href="#">Déconnexion</a></li>
+            </ul>
+        </li>
+    </div>
 </nav>
