a9325219850624fa891459b76afd9cd23cc9e248
FlorentBuhot <florent.buhot@etu.uca.fr>
2024-02-18 14:26:09 +0100

    Update inscription.php

--- php/pages/inscription.php
+++ php/pages/inscription.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-    <?php
-    include_once("/var/www/html/php/template/inc_header.php");
-    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de connexion</title>
