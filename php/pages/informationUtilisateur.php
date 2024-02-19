876a3701ba020f2f48a3d0613dec18cedcd11fba
Dhaisne <dhaisneclement@gmail.com>
2024-02-19 14:00:41 +0100

    Ajout front + back

--- php/pages/informationUtilisateur.php
+++ php/pages/informationUtilisateur.php
@@ -1,80 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php
-    include_once("php/template/inc_head.php");
-    ?>
-    <title>Mon profil</title>
-</head>
-<body class="container">
 <?php
-include_once("php/template/inc_header.php");
-?>
+    require_once("php/template/inc_connexionBase.php");
+
+    //Requete pour récupérer toutes les équipes
+    $texteReq = "select * ";
+    $texteReq .= "from user ";
+    $texteReq .= "where login = :login";
+
+    $requete = $cnx->prepare($texteReq);
+    $requete->bindParam(':login', $_SESSION['login']);
+    $requete->execute();
+    $tabUser = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    //Requete pour récupérer toutes les équipes
+    $texteReq = "select * ";
+    $texteReq .= "from joueur ";
+    $texteReq .= "where user_id = :user_id";
+
+    $requete = $cnx->prepare($texteReq);
+    $requete->bindParam(':user_id', $_SESSION['user_id']);
+    $requete->execute();
+    $tabJoueur = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    // Get le joueur en cours
+    $texteReq = "select * ";
+    $texteReq .= "from joueur ";
+    $texteReq .= "where user_id = :user_id";
+
+    //demander la creation de la requete à l'instance PDO ($cnx)
+    $requete = $cnx->prepare($texteReq);
+    $requete->bindParam(':user_id', $_SESSION['user_id']);
+    $requete->execute();
+    $currentJoueur = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    // Get le nombre de match pour ce joueur
+    $texteReq = "select count(*) ";
+    $texteReq .= "from matchs ";
+    $texteReq .= "where id_equipe_1 = (select equipe_id from equipe where (id_joueur1 = :id_joueur or id_joueur2 = :id_joueur or id_joueur3 = :id_joueur or id_joueur4 = :id_joueur or id_joueur5 = :id_joueur)) ";
+    $texteReq .= "or id_equipe_2 = (select equipe_id from equipe where (id_joueur1 = :id_joueur or id_joueur2 = :id_joueur or id_joueur3 = :id_joueur or id_joueur4 = :id_joueur or id_joueur5 = :id_joueur))";
 
 
-<h1 class="mt-5">Utilisateur : LE NOM DE L'UTILISATEUR</h1>
-<div class="d-flex justify-content-center">
-    <button type="submit" class="btn btn-success mt-4">Retourner à l'accueil</button>
-</div>
-<form>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Login :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
-    <br/>
-    <div class="input-group input-group-lg">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Mot de passe :</span>
-        <input type="password" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg">
-    </div>
-    <div>
-        <br/>
-        <label for="start">Rôle de l'équipe :</label>
-        <select class="form-select" aria-label="Default select">
-            <option selected>LE ROLE ACTUEL</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-        </select>
-    </div>
-    <div class="input-group input-group-lg">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Nom :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg">
-    </div>
-    <div class="input-group input-group-lg">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Prénom :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg">
-    </div>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text fw-bold" id="inputGroup-sizing-lg">Score :</span>
-        <input type="text" class="form-control fw-bold" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Nombre de match :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Nombre de but :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Nombre d'arret :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
-    <div class="input-group input-group-lg mt-3">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Nombre de passe décisive :</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-lg" readonly>
-    </div>
+    $requete = $cnx->prepare($texteReq);
+    $requete->bindParam(':id_joueur', $currentJoueur[0]['joueur_id']);
+    $requete->execute();
+    $nbMatchJoueur = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-    <button type="submit" class="btn btn-success mt-4">Enregistrer</button><br/><br/>
-</form>
-</body>
-</html>
+    $score = ($tabJoueur[0]["nb_but"] * 100 + $tabJoueur[0]["nb_arret"] * 75 + $tabJoueur[0]["nb_passe_de"] * 50) / $nbMatchJoueur[0]['count(*)'];
+
+    require_once("php/view/profil.php");
